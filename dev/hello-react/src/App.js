7379d9800e43845a7bc55e2be61de62426b457c5
gugucon-hwan <gugucon2018@gmail.com>
2020-09-13 23:02:33 +0900

    리액트

--- dev/hello-react/src/App.js
+++ dev/hello-react/src/App.js
@@ -1,31 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name='리액트';
-  return (
-    <>
-    <h1>{name}리액트 안녕!</h1>
-    <h2>잘 작동하니?</h2>
-    </>
-/*    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
-  );
-}
+const App = () => {
+  return <MyComponent>리액트</MyComponent>;
+};
 
 export default App;
