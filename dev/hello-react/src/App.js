f23b618be6e5123c6d0391138f4bb80f16bea0e6
gugucon-hwan <gugucon2018@gmail.com>
2020-09-16 21:46:04 +0900

    진도 나감

--- dev/hello-react/src/App.js
+++ dev/hello-react/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import EventPractice from './EventPractice';
 //import Counter from './Counter';
-import Say from './Say';
+//import Say from './Say';
 //import logo from './logo.svg';
 //import './App.css';
 //import MyComponent from './MyComponent';
 
 const App = () => {
-  return <Say />;
+  return <EventPractice />;
 };
 
 export default App;
